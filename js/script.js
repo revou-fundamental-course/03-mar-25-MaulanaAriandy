d9a64a7acacae858f6947155bda5ce71703010c0
MaulanaAriandy <maulanaariandy@gmail.com>
2025-03-10 08:52:25 +0700

    carousel added, message us added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // Ini file javascript
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("banner-img");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block"
+    setTimeout(carousel, 2000)
+}
